798f29e43140ec4e4567aa2dba03e19a6d51d2a3
Hallison Oliveira da Paz <hallison.paz@SJ208F34.local>
2018-12-06 22:43:47 -0200

    nagivation models implemented

--- app/src/main/java/io/github/mobileteacher/imagestoragesample/MainActivity.java
+++ app/src/main/java/io/github/mobileteacher/imagestoragesample/MainActivity.java
@@ -6,12 +6,17 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
@@ -29,13 +34,18 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-    RecyclerView recyclerView;
     final int CAMERA_REQUEST_CODE = 71;
 
     StorageReference storageReference = FirebaseStorage.getInstance().getReference("imagens");
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("imagens");
+
+    ImageListFragment imageListFragment;
+    TextFragment textFragment;
+    ImageFragment imageFragment;
+
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
 
         ActionBar actionBar = getSupportActionBar();
         //actionBar.up
@@ -62,9 +73,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setAdapter(new PhotoAdapter());
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        imageListFragment = new ImageListFragment();
+        textFragment = TextFragment.newInstance("Sucesso absoluto");
+        imageFragment = new ImageFragment();
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.container, imageListFragment);
+        ft.commit();
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -136,5 +156,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(Gravity.START)){
+            drawerLayout.closeDrawer(Gravity.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (drawerLayout.isDrawerOpen(Gravity.START)){
+            drawerLayout.closeDrawer(Gravity.START);
+        }
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Fragment currentFragment;
+
+        switch (item.getItemId()){
+            case R.id.photo_list_item:
+                currentFragment = imageListFragment;
+                break;
+            case R.id.text_item:
+                currentFragment = textFragment;
+                break;
+            case R.id.image_item:
+                currentFragment = imageFragment;
+                break;
+            default:
+                currentFragment = imageListFragment;
+        }
+
+        transaction.replace(R.id.container, currentFragment);
+        transaction.commit();
+        return true;
     }
 }
